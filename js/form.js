0572de1fdb760bb2b334bf3abb019d812e801a66
Daniel Gonzalez Reina <dangonrei@gmail.com>
2016-11-26 12:46:18 +0100

    Finished the contact section. Pending to make it responsive

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@ let $btn;
 let $name; // referenced to the wrapper, not the input
 let $email; // referenced to the wrapper, not the input
 let $message;
+let $alert;
 let inputReady = false;
 
 $().ready(args => {
@@ -11,13 +12,20 @@ $().ready(args => {
   $name = $('#name-form');
   $email = $('#email-form');
   $message = $('#message-form');
+  $alertContainer = $('#alert-container');
+  $alert = $('#contact-alert');
+  $alertContent = $('#alert-content');
 
   $('#btnSend').click(sendClick);
+
+  $('#alert-btn-close').click(closeAlert);
 });
 
 const sendClick = e => {
+  console.log('entra en sendClick');
 
   e.preventDefault();
+  closeAlert();
 
   inputReady = checksErrors();
 
@@ -27,12 +35,12 @@ const sendClick = e => {
     let name = $name.children('input').val();
     let email = $email.children('input').val();
 
-    $.ajax({
-      url: "https://formspree.io/dangonrei@gmail.com", 
-      method: "POST",
-      data: $('#form').serialize(),
-      dataType: "json",
-    });
+    let formspreeUrl = "https://formspree.io/dangonrei@gmail.com";
+    let formspreeData = $('#form').serialize();
+
+    $.post(formspreeUrl, formspreeData, "json")
+      .done(messageSentSuccess)
+      .fail(messageNotSent);
   }
 
 }
@@ -69,9 +77,23 @@ const checksErrors = $wrapper => {
     }
   }
 
-  // TODO handle a notification when we have errors
+  // If the form has error, show an alert explaining it
+  let hasError = inputsWithError.length == 0? false : true;
+  if (hasError) {
+    let multiError = inputsWithError.length > 1;
+    let endingS = multiError? 's ' : ' ';
+    let errorMessage = 'There ' + (multiError? 'are some ' : 'is an ') +
+        'error' + endingS + 'in the field' + endingS +
+        inputsWithError.join(' and ');
+
+    // Show an alert
+    $alertContainer.addClass('visible');
+    $alert.addClass('alert-danger');
+    $alertContent.html(errorMessage);
+  }
   
-  return inputsWithError.length == 0? true : false;
+  // returns true if checksError doesn't find an error (i.e. hasError = false)
+  return !hasError;
 }
 
 const inputError = $wrapper => {
@@ -81,7 +103,7 @@ const inputError = $wrapper => {
 
   // Add a change listener to handle changes
   // If the input is wrong and the user changes it, the error is removed
-  // But, if the input us unfocused and the value is wrong, the error appears again
+  // But, if the input is unfocused and the value is wrong, the error appears again
   $wrapperInput.off('keyup').off('focusout');
   $wrapperInput.keyup(function(e) {
     if($(this).val() != '') {
@@ -91,5 +113,44 @@ const inputError = $wrapper => {
   $wrapperInput.focusout(checksErrors.bind(this, $wrapper));
 
 }
+
+const messageSentSuccess = () => {
+  console.log('messageSentSuccess');
+
+  // clear the inputs
+  $name.children('input').val('');
+  $email.children('input').val('');
+  $message.children('textarea').val('');
+
+  // show the alert with the success message
+  $alertContainer.addClass('visible');
+  $alert.addClass('alert-success');
+  $alertContent.html(
+    'Your message was sent successfully. ' +
+    'I will write you back as soon as possible. ' +
+    'Thank you for contacting me! :)'
+  );
+}
+
+const messageNotSent = () => {
+  // show the alert
+  $alertContainer.addClass('visible');
+  $alert.addClass('alert-danger');
+  $alertContent.html(
+    'There was a problem sending your message.'+
+    ' You can try again or email me directly at <b>dangonrei@gmail.com</b>.'+
+    ' Sorry for the inconvenience.'
+  );
+}
+
+const closeAlert = () => {
+  console.log('closeAlert');
+  $alertContainer.removeClass('visible alert-success alert-danger');
+  $alert.removeClass('alert-success alert-danger');
+  $alertContent.val('');
+}
+
+// TODO animation for closing alert?
+// TODO shadows in the alert?
 
 })(jQuery)
