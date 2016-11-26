eabecae1ce6ccaa301a513b28b56e8e87c52b790
Daniel Gonzalez Reina <dangonrei@gmail.com>
2016-11-26 18:18:54 +0100

    Improved the position linked from the menu

--- js/index.js
+++ js/index.js
@@ -3,8 +3,11 @@ $().ready(args => {
   console.log('ready');
 
   $('.navlink').click(function() {
+    let linkedId = $(this).attr('href');
+    let correction = linkedId == '#about'? 0 : 80;
+
     $('html, body').animate({
-      scrollTop: $( $(this).attr('href') ).offset().top
+      scrollTop: $(linkedId).offset().top - correction
     }, 500);
     return false;
   });
